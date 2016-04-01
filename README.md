6f98b135f1506e2cc53b3c1c5cbd289ae8d19f1f
Douglas Fils <dfils@oceanleadership.org>
2016-04-01 07:03:25 -0500

    First commit of the ocdCommons package

---
+++ README.md
@@ -0,0 +1,5 @@
+Place for common structs and functions used by the other ocd Applications.
+This commons set will become more and more required for the other apps to run 
+from.
+
+
---
+++ structs/expedition.go
@@ -0,0 +1,30 @@
+package structs
+
+// used in ocdBulk and ocdServices
+type Expedition struct {
+	Uri                    string
+	Lat                    string
+	Long                   string
+	Hole                   string
+	Expedition             string
+	Site                   string
+	Program                string
+	Waterdepth             string
+	CoreCount              string
+	Initialreportvolume    string
+	Coredata               string
+	Logdata                string
+	Geom                   string
+	Scientificprospectus   string
+	CoreRecovery           string
+	Penetration            string
+	Scientificreportvolume string
+	Expeditionsite         string
+	Preliminaryreport      string
+	CoreInterval           string
+	PercentRecovery        string
+	Drilled                string
+	Vcdata                 string
+	Note                   string
+	Prcoeedingreport       string
+}
